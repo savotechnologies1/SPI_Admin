3a1b87f2e79aedc93e3886b2246ea072e94a974c
savotechnologies1 <git.stpl@gmail.com>
2025-06-03 12:31:40 +0530

    updated code

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -28,25 +28,25 @@ const SignIn = () => {
     password: string;
   }
 
-  // const onSubmit = async (data: FormData) => {
-  //   try {
-  //     const response = await loginApi(data);
-  //     console.log("responseresponse", response);
-  //     if (response.status === 200) {
-  //       login(response.data.token); 
-  //       console.log("login page redirect");
-  //       navigate("/", { replace: true });
-  //     }
-  //   } catch (error: unknown) {
-  //     toast.error(error.response.message);
-  //   }
-  // };
   const onSubmit = async (data: FormData) => {
-  // Mock login logic
-  login("mockToken"); // just pass a fake token
-  console.log("Redirecting to dashboard without backend validation");
-  navigate("/", { replace: true });
-};
+    try {
+      const response = await loginApi(data);
+      console.log("responseresponse", response);
+      if (response.status === 200) {
+        login(response.data.token); 
+        console.log("login page redirect");
+        navigate("/", { replace: true });
+      }
+    } catch (error: unknown) {
+      toast.error(error.response.message);
+    }
+  };
+//   const onSubmit = async (data: FormData) => {
+//   // Mock login logic
+//   login("mockToken"); // just pass a fake token
+//   console.log("Redirecting to dashboard without backend validation");
+//   navigate("/", { replace: true });
+// };
 
 
   return (
--- src/pages/dashboard/DasboardDetails.tsx
+++ src/pages/dashboard/DasboardDetails.tsx
@@ -151,3 +151,5 @@ const DasboardDetails = () => {
 };
 
 export default DasboardDetails;
+
+
